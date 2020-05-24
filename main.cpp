9c0493a7b92346926253c7203ef4f1fdb599d3ba
Paul <48963940+PaulCooz@users.noreply.github.com>
2020-05-24 21:03:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,32 +2,41 @@
 #include <complex>
 #include <iostream>
 
-#define ll long long
-#define ld long double
-
 using namespace std;
 
-ld Lr, Li, Rr, Ri, steps;
-ll Pow;
+long double Lr, Li, Rr, Ri, PixelsPerDistance;
+int Pow, hmp;
+
+void HowMuchHasPassed(int r)
+{
+    if (r % 10 == 0 && r != hmp)
+    {
+        hmp = r;
+        cout << hmp << "%" << endl;
+    }
+}
 
 void MakeImage()
 {
-    const int MW = (Rr - Lr) * steps + 2,
-              MH = (Ri - Li) * steps + 2;
-    QImage img(MW, MH, QImage::Format_RGB32);
-    
-    ld IncR = 0.8 / steps,
-       IncI = 0.8 / steps;
-    for(ld r = Lr; r < Rr; r += IncR)
+    const int MaxW = (Rr - Lr) * PixelsPerDistance + 2,
+              MaxH = (Ri - Li) * PixelsPerDistance + 2;
+    QImage img(MaxW, MaxH, QImage::Format_RGB32);
+
+    const long double OnePercent = (Rr - Lr) / 100.0;
+    hmp = -1;
+
+    long double IncR = 0.8 / PixelsPerDistance,
+                IncI = 0.8 / PixelsPerDistance;
+    for(long double r = Lr; r < Rr; r += IncR)
     {
-        for(ld i = Li; i < Ri; i += IncI)
+        for(long double i = Li; i < Ri; i += IncI)
         {
-            complex<ld> c(r, i), z(0, 0);
+            complex<long double> c(r, i), z(0, 0);
 
             int cr = 0, cg = 0, cb = 0;
             for(int it = 0; it < 255; it++)
             {
-                if (it % 3 == 0) cr = it;
+                if (it % 3 == 0) cr = it;                               // Set color
                 else if (it % 3 == 1) cg = it;
                 else if (it % 3 == 2) cb = it;
 
@@ -36,14 +45,19 @@ void MakeImage()
                 if (abs(z) > 2) break;
             }
 
-            ll x = abs(Lr) * steps + r * steps,
-               y = abs(Li) * steps + i * steps;
+            int x = r * PixelsPerDistance;                              // Set x
+            x -= Lr * PixelsPerDistance;                                // 0 <= x <= MaxW
 
-            img.setPixel(x, y, qRgb(4*cr, 4*cg, 4*cb));
+            int y = i * PixelsPerDistance;                              // Set y
+            y -= Li * PixelsPerDistance;                                // 0 <= y <= MaxH
+
+            img.setPixel(x, y, qRgb(4 * cr, 4 * cg, 4 * cb));           // Set a color pixel
         }
+
+        HowMuchHasPassed((r - Lr) / OnePercent);                        // Output hmp
     }
 
-    img.save("MandelbrotSet.png", "PNG");
+    img.save("MandelbrotSet.png", "PNG");                               // Make new image
 }
 
 
@@ -59,10 +73,12 @@ int main()
     cout << "Input max imag" << endl;
     cin >> Ri;
 
-    steps = 500.0 / max((Rr - Lr), (Ri - Li));
-
     cout << "Input power" << endl;
     cin >> Pow;
+
+    cout << "Number of pixels" << endl;
+    cin >> PixelsPerDistance;
+    PixelsPerDistance /= max((Rr - Lr), (Ri - Li));
 
     MakeImage();
 }
